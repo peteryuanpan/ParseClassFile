19f937cfe48d7413199cbc7550b75b4f2e547a24
peteryuanpan <peteryuanpan@gmail.com>
2020-10-10 16:36:15 +0800

    20201010

--- src/main/resources/classfile/IntegerNumberDemo.java
+++ src/main/resources/classfile/IntegerNumberDemo.java
@@ -8,8 +8,10 @@ public class IntegerNumberDemo {
     int a4 = 32768;
     int a5 = 123456789;
 
-    int a6 = -32767;
-    int a7 = -32768;
+    int a6 = -1;
+    int a7 = -32767;
+    int a8 = -32768;
+    int a9 = -12345678;
 
     boolean bol1 = true;
     byte by1 = 13;
