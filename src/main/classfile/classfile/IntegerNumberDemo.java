07edb82c1767dee1a68b72368973329c08e1e32f
peteryuanpan <peteryuanpan@gmail.com>
2020-10-10 15:56:41 +0800

    20201010

---
+++ src/main/classfile/classfile/IntegerNumberDemo.java
@@ -0,0 +1,18 @@
+package classfile;
+
+public class IntegerNumberDemo {
+
+    int a1 = 123;
+    final int a2 = 124;
+    int a3 = 32767;
+    int a4 = 32768;
+    int a5 = 123456789;
+
+    boolean bol1 = true;
+    byte by1 = 13;
+    char ch1 = '1';
+    short sh1 = 30000;
+
+    public static void main(String[] args) {
+    }
+}
